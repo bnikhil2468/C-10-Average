0fe187dbf611e3d224c5fc568250a7d4632da20f
bnikhil2468 <92759981+bnikhil2468@users.noreply.github.com>
2021-10-31 17:13:50 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+function setup() 
+{
+  createCanvas(400,400);
+
+  var weight = [35,32,45,50,37];
+  var sum = weight[0]+weight[1]+weight[2]+weight[3]+weight[4];
+  var avg = sum/weight.length;
+  console.log(avg);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
